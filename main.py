496016552128e1567cc47fdab638d4d8297d7ad8
selvaggi <michele.selvaggi@cern.ch>
2020-05-11 11:30:32 +0200

    last commit

--- main.py
+++ main.py
@@ -34,7 +34,7 @@ def main():
 
     #isolines = [100, 10, 3]
     isolines = [100,10,3]
-    npoints = 20
+    npoints = 10
 
     ### B meson branching ratios
     meson_br_B0 = 0.43
@@ -42,26 +42,60 @@ def main():
     meson_br_Bs = 0.10
     meson_br_Bc = 8.5e-04
 
+    ## acceptance in eta region for different pt cuts (to be assessed in sim)
+    nose_acceptance = dict()
+    nose_acceptance[5.] = 0.141
+    nose_acceptance[10.] = 0.117
+    nose_acceptance[20.] = 0.071
+
+    hgc_acceptance = dict()
+    hgc_acceptance[5.] = 0.356
+    hgc_acceptance[10.] = 0.335
+    hgc_acceptance[20.] = 0.31
+
+
     class Parameters(object):
         ns0=1.e15   ## number of bb pairs at HL-LHC
-        trigger_eff=1   ## B-parking / or whatever other cuts needed to trigger
-        eta_eff=0.1 ## acceptance for the Bmeson to go in particular direction
-
-        llp_br='data/br_BpD0str_eXN.csv'
+        trigger_eff=0.01   ## B-parking / or whatever other cuts needed to trigger
+        #eta_eff=nose_acceptance[5.] ## acceptance for the Bmeson to go in particular direction
+        pt=10.
+        eta_eff=hgc_acceptance[pt] ## acceptance for the Bmeson to go in particular direction
 
         ## fraction of Bmesons produced in particular mode
         list_brs = [
-                    (meson_br_Bp,'data/br_BpD0str_eXN.csv'),
-                    (meson_br_Bp,'data/br_Bp_eN.csv'),
-                    (meson_br_B0,'data/br_B0_pipeN.csv'),
-                    (meson_br_Bc,'data/br_Bc_eXN.csv'),
+                        (meson_br_Bp,'data/br_Bp_eN.csv'),
+                        (meson_br_Bp,'data/br_Bp_D0eN.csv'),
+                        (meson_br_Bp,'data/br_Bp_D0steN.csv'),
+                        (meson_br_Bp,'data/br_Bp_pi0eN.csv'),
+                        (meson_br_Bp,'data/br_Bp_rho0eN.csv'),
+                        (meson_br_B0,'data/br_B0_DpeN.csv'),
+                        (meson_br_B0,'data/br_B0_rhopeN.csv'),
+                        (meson_br_B0,'data/br_B0_pipeN.csv'),
+                        (meson_br_B0,'data/br_B0_DpsteN.csv'),
+                        (meson_br_Bs,'data/br_Bs_DssteN.csv'),
+                        (meson_br_Bs,'data/br_Bs_KpsteN.csv'),
+                        (meson_br_Bs,'data/br_Bs_KpeN.csv'),
+                        (meson_br_Bs,'data/br_Bs_DseN.csv'),
+                        (meson_br_Bc,'data/br_Bc_eN.csv')
                    ]
 
+
+        '''
+        ### Nose
         r_min=0.25
         r_max=1.05
         z_min=10.
         z_max=10.5
-        pt=5.
+        '''
+
+        ## HGC
+        r_min=0.40
+        r_max=2.20
+        z_min=300.
+        z_max=520.
+
+
+        pt=10.
         name='Nose'
         color='red'
         alpha=0.2
